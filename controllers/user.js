5b125d960cedaf6d63974a1f5607c54e4845073d
satyasai002 <satyasaimadarapu@gmail.com>
2022-11-12 01:01:35 +0530

    v1

--- controllers/user.js
+++ controllers/user.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
-const User = require("../models/user");
+const User = require("../models/User");
 
 exports.user_signup = (req, res) => {
   User.find({ email: req.body.email })
