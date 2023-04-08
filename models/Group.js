36ba5fa239b8c91022c78eac1e812b0c94a8649e
satyasai002 <satyasaimadarapu@gmai.com>
2023-04-08 20:19:50 +0530

    added time stamp

--- models/Group.js
+++ models/Group.js
@@ -1,20 +1,27 @@
 const mongoose = require("mongoose");
 
-const groupSchema = new mongoose.Schema({
-  _id: mongoose.Schema.Types.ObjectId,
-  groupName: {
-    type: String,
-    required: true,
+const groupSchema = new mongoose.Schema(
+  {
+    _id: mongoose.Schema.Types.ObjectId,
+    groupName: {
+      type: String,
+      required: true,
+    },
+    admin: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    members: [
+      {
+        type: mongoose.Types.ObjectId,
+        ref: "User",
+      },
+    ],
   },
-  admin:{
-    type:mongoose.Schema.Types.ObjectId,
-    required:true,
-    ref: "User",
-  },
-  members: [{
-    type: mongoose.Types.ObjectId,
-    ref: "User",
-  }],
-});
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("Group", groupSchema);
--- models/User.js
+++ models/User.js
@@ -1,25 +1,30 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  _id: mongoose.Schema.Types.ObjectId,
-  fullName: {
-    type: String,
-    required: true,
+const userSchema = new mongoose.Schema(
+  {
+    _id: mongoose.Schema.Types.ObjectId,
+    fullName: {
+      type: String,
+      required: true,
+    },
+    mobile: {
+      type: Number,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
-  mobile: {
-    type: Number,
-    required: true,
-    unique: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("User", userSchema);
